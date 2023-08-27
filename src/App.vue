595b2599c8596f87189a1e62f7f16cbe1da96d73
hwanglast <haryantowang09@gmail.com>
2023-08-27 22:19:59 +0700

    Initial Commit + apply ServerItems Component

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <header>
+                    <h1>Server Status</h1>
+                </header>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <server-items :items="5"></server-items>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <p>Server Details are currently not updated</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12">
+                <footer>
+                    <p>All Servers are managed here</p>
+                </footer>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import ServerItems from './components/ServerItems.vue';
+
+export default {
+    components: {
+        ServerItems,
+    },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ServerItems.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <ul class="list-group">
+      <li
+        class="list-group-item"
+        v-for="index in items">
+        Server #{{ index }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['items'],
+}
+</script>
